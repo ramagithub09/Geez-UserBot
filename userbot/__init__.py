2f6953fef04a2e1d8753d729c99ae3cdea8a50c9
ramagithub09 <84198023+ramagithub09@users.noreply.github.com>
2021-05-30 06:33:46 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -106,7 +106,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/vckyou/Geez-UserBot")
+    "https://github.com/ramagithub09/Rawan2-UserBot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Geez-UserBot")
 
